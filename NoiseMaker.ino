bfa65769da4179075076530163169daf11159f92
olibrian <mail@obrian.ch>
2019-12-03 20:00:27 +0100

    Add Keypad function

--- NoiseMaker.ino
+++ NoiseMaker.ino
@@ -6,6 +6,7 @@
   circuit:
   - 8 ohm speaker on digital pin 8
   - Potentiometer on analog pin 0
+  - Keypad pin 2,3,4,5,6,8,9,10
 
   created 01 Dez 2019
   modified 30 Aug 2011
@@ -21,10 +22,34 @@
 #include <SoftwareSerial.h>
 
 
+// Include Keypad Library
+#include <Key.h>
+#include <Keypad.h>
+
 // Potentiometer sensor
 #define SENSOR A0
 
 #define DEBUG true
+
+// ** Keypad
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'a'},
+  {'4', '5', '6', 'b'},
+  {'7', '8', '9', 'c'},
+  {'d', 'p', 'u', 'd'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 9, 6, 4, 2 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 10, 8, 5, 3 };
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+// ** End keypad
 
 
 // notes in the melody:
@@ -44,10 +69,10 @@ void setup() {
 
 void loop() {
   // get sensor for speed
-  int vx = analogRead(SENSOR);
-  vx = vx / 180;
+  int pitch = analogRead(SENSOR);
+  pitch = pitch / 180;
 
-  debug("Sensor=" + String(vx));
+  debug("Sensor=" + String(pitch));
 
   
   // iterate over the notes of the melody:
@@ -55,7 +80,7 @@ void loop() {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 500 / noteDurations[thisNote] * vx;
+    int noteDuration = 500 / noteDurations[thisNote] * pitch;
     tone(8, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
